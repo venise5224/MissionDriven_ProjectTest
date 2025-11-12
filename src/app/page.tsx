cdf3992cf77a52bb6bcadfd1d092aea34fb5855b
JSM <JSM@>
2025-11-12 18:51:47 +0900

    feat:메인이미지업로더UI PC버전 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import MainImageUploader from "@/components/MainImageUploader";
+
 export default function Home() {
   return (
-    <main className="md:max-w-[1100px] mx-auto px-4 md:px-5">과제 페이지</main>
+    <main className="md:max-w-[1100px] mx-auto px-4 md:px-5">
+      <div className="mt-10">
+        <MainImageUploader />
+      </div>
+    </main>
   );
 }
