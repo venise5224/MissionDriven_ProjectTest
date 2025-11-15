1d843e76deab351263a51b8a928d539f2cb79b25
JSM <JSM@>
2025-11-15 18:12:47 +0900

    카테고리선택 기능 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/layout/Header";
+import { Toaster } from "react-hot-toast";
 
 export const metadata: Metadata = {
   title: "Misstion Driven FE Project Test",
@@ -15,6 +16,20 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body>
+        <Toaster
+          position="bottom-center"
+          containerStyle={{
+            bottom: "80px",
+          }}
+          toastOptions={{
+            style: {
+              background: "#323232",
+              color: "#fff",
+              width: "100%",
+              maxWidth: "520px",
+            },
+          }}
+        />
         <Header />
         {children}
       </body>
